345f8a06fe3489d1da29d62b8908d1ce049c1022
Masum <masumbarot77@gmail.com>
2025-08-04 10:56:02 +0530

    hii

---
+++ src/practical 1.kt
@@ -0,0 +1,26 @@
+fun main(){
+   /* print("Enter second number: ")
+        print("Enter first number: ")
+        val x:String= readln()
+    var p:Int=  x.toInt()
+
+
+        val y:String = readln()
+    var q:Int=y.toInt()
+    print("Enter operator (+, -, *, /): ")
+    val operator= readLine()
+
+        val result = when (operator) {
+            "+" -> p + q
+            "-" -> p - q
+            "*" -> p * q
+            "/" -> p/q
+            else -> "Invalid operator"
+        }
+
+        println("Result: $result")
+
+    */
+
+
+    }
---
+++ src/practical1.2.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+        var intVal:Int = 10
+        println("Integer Value : ${intVal}")
+        println("Double Value (From Integer): ${intVal.toDouble()}")
+
+        var stringVal:String = "10"
+        println("String Value : ${stringVal}")
+        println("Integer Value1 (From String) : ${stringVal.toInt()}")
+        println("Integer Value2 (From String) : ${stringVal.toInt()}")
+
+        var stringVal2:String = "10.12"
+        println("Double Value (From String) : ${stringVal2.toDouble()}")
+
+}
