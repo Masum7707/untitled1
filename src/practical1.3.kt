0b3ce7619217d731102066e5792d72e22b2fc33c
Masum <masumbarot77@gmail.com>
2025-08-04 11:05:41 +0530

    hii

---
+++ src/practical1.3.kt
@@ -0,0 +1,23 @@
+fun main() {
+    print("Enter student's name: ")
+    val name = readLine()
+
+    print("Enter enrollment number: ")
+    val enrollment = readLine()
+
+    print("Enter branch: ")
+    val branch = readLine()
+
+    print("Enter age: ")
+    val age = readLine()
+
+    print("Enter semester: ")
+    val semester = readLine()
+
+    println("\n--- Student Information ---")
+    println("Name: $name")
+    println("Enrollment No: $enrollment")
+    println("Branch: $branch")
+    println("Age: $age")
+    println("Semester: $semester")
+}
